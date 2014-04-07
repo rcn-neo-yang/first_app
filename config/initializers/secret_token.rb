08cfb1877081539ea2ac4f718b4e637ef1288316
rcn-neo-yang <rcn.neo.yang@gmail.com>
2014-04-07 16:29:55 +0800

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '797d28f399c7d8269d815a84cf8ef2e77cec092a90cf9a08af58e765073124bf8f53d669f5e4d2d426359974055b2d78f4e5256d0cef14ae70f2a8b537a8c8f7'
